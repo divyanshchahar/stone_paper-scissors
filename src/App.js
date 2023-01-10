6553b95fa7ccde80d252cb238a0452482b7eca93
divyanshchahar <divyanshchahar@outlook.com>
2023-01-10 15:59:28 +0530

    added componenent to select number of rounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import NumberOfRounds from "./components/NumberOfRounds";
+
 function App() {
   return (
     <>
       <h1>Stone Paper Scissors Emulator</h1>
+      <NumberOfRounds />
     </>
   );
 }
